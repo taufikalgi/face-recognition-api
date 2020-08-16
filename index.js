95a139bfc765e2b24e139b2cd9cdecc13d42781a
Taufik <taufik.algi99@gmail.com>
2020-08-16 19:51:26 +0700

    Revert "change / response"
    This reverts commit 3eef5e5a9648b24ea0ceff15ee9eebdbcd9d5a9b.

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hi there!");
+  res.send(database.users);
 });
 
 app.post("/signin", (req, res) => signin.signinHandler(req, res, pg, bcrypt));
