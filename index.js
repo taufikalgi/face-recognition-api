df8b3a5a2fb0de5747da612dacca6d0236709adb
Taufik <taufik.algi99@gmail.com>
2020-08-16 21:32:27 +0700

    change connectionString

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const cors = require("cors");
 const pg = require("knex")({
   client: "pg",
   connection: {
-    connectionString:
-      process.env.DATABASE_URL ||
-      "postgres://uxnyubvlnbqedy:a5661eb0dbf2ec5add75ebafb949163a2247a4e936823b7171579fdee31580f4@ec2-34-195-115-225.compute-1.amazonaws.com:5432/d764a950pgl4ku",
+    connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     },
